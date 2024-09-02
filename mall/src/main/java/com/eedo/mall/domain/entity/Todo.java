115309012b543a85875cb5e0b3cbe45d8b0407f6
eedo_y <doup2001@naver.com>
2024-09-02 17:08:06 +0900

    add : Entity & Repository 추가

---
+++ mall/src/main/java/com/eedo/mall/domain/entity/Todo.java
@@ -0,0 +1,51 @@
+package com.eedo.mall.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "todo_Table")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long tno;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    private String content;
+
+    private boolean complete;
+
+    private LocalDate localDate;
+
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
+    public void changeComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public void changeLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+
+
+}
---
+++ mall/src/main/java/com/eedo/mall/domain/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.eedo.mall.domain.repository;
+
+import com.eedo.mall.domain.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+
+
+}
