88f714eb7da770270f069b14eecf8f0ef57de4ad
eedo_y <doup2001@naver.com>
2024-09-03 13:39:28 +0900

    add : ServiceImpl 메서드 추가

--- mall/src/main/java/com/eedo/mall/domain/service/TodoServiceImpl.java
+++ mall/src/main/java/com/eedo/mall/domain/service/TodoServiceImpl.java
@@ -23,6 +23,39 @@ public class TodoServiceImpl implements TodoService{
 
         return entityToDTO(todo);
     }
+
+    @Override
+    public Long register(TodoDTO dto) {
+
+        Todo result = dtoToEntity(dto);
+
+        Todo todo = todoRepository.save(result);
+        return todo.getTno();
+    }
+
+    @Override
+    public void modify(TodoDTO dto) {
+
+        Optional<Todo> result = todoRepository.findById(dto.getTno());
+        Todo todo = result.orElseThrow();
+
+        todo.changeTitle(dto.getTitle());
+        todo.changeContent(dto.getContent());
+        todo.changeLocalDate(dto.getLocalDate());
+
+        todoRepository.save(todo);
+
+    }
+
+    @Override
+    public void remove(Long tno) {
+        Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElseThrow();
+
+        todoRepository.delete(todo);
+
+    }
+
     @Override
     public TodoDTO entityToDTO(Todo todo) {
         return TodoService.super.entityToDTO(todo);
