e24ed4f07c41d59ed9950ec6ab87c1f8b7edd1af
eedo_y <doup2001@naver.com>
2024-09-04 13:00:57 +0900

    fix : TodoSearch & TodoSearchImpl 내용 추가 QueryDsl

--- mall/src/main/java/com/eedo/mall/domain/repository/search/TodoSearch.java
+++ mall/src/main/java/com/eedo/mall/domain/repository/search/TodoSearch.java
@@ -1,10 +1,12 @@
 package com.eedo.mall.domain.repository.search;
 
+import com.eedo.mall.domain.dto.PageRequestDTO;
+import com.eedo.mall.domain.dto.PageResponseDTO;
 import com.eedo.mall.domain.entity.Todo;
 import org.springframework.data.domain.Page;
 
 public interface TodoSearch {
 
-    Page<Todo> search1();
+    Page<Todo> search1(PageRequestDTO pageRequestDTO);
 
 }
--- mall/src/main/java/com/eedo/mall/domain/repository/search/TodoSearchImpl.java
+++ mall/src/main/java/com/eedo/mall/domain/repository/search/TodoSearchImpl.java
@@ -1,15 +1,14 @@
 package com.eedo.mall.domain.repository.search;
 
+import com.eedo.mall.domain.dto.PageRequestDTO;
 import com.eedo.mall.domain.entity.QTodo;
 import com.eedo.mall.domain.entity.Todo;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.support.Querydsl;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
 
 @Slf4j
 public class TodoSearchImpl extends QuerydslRepositorySupport implements TodoSearch {
@@ -19,21 +18,22 @@ public class TodoSearchImpl extends QuerydslRepositorySupport implements TodoSea
     }
 
     @Override
-    public Page<Todo> search1() {
+    public Page<Todo> search1(PageRequestDTO pageRequestDTO) {
         log.info("Search1...");
         QTodo todo = QTodo.todo;
 
         JPQLQuery<Todo> query = from(todo);
 
-        query.where(todo.title.contains("1"));
-        query.fetch(); // fetch하면 실행가능
+//        query.where(todo.title.contains("1"));
 
         //페이징 처리가 달라졌다
-        Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+        Pageable pageable = PageRequest.of(pageRequestDTO.getPage()-1, pageRequestDTO.getSize(), Sort.by("tno").descending());
         this.getQuerydsl().applyPagination(pageable, query);
 
+        List<Todo> list = query.fetch();// fetch하면 실행가능
+        long total = query.fetchCount();
 
-        return null;
+        return new PageImpl<>(list, pageable, total);
 
     }
 
