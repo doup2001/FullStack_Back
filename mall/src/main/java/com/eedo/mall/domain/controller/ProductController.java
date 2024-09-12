0aaa06d52adca7355227a6f390ab8ddd9883cf15
eedo_y <doup2001@naver.com>
2024-09-12 10:21:24 +0900

    feat :read 기능 추가

--- mall/src/main/java/com/eedo/mall/domain/controller/ProductController.java
+++ mall/src/main/java/com/eedo/mall/domain/controller/ProductController.java
@@ -14,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @Log4j2
@@ -23,24 +25,6 @@ public class ProductController {
 
     private final CustomFileUtil FileUtil;
     private final ProductService productService;
-
-
-    @PostMapping
-    public Map<String, String> register(ProductDTO productDTO) {
-
-        log.info("[My LOG] : register " + productDTO);
-
-        List<MultipartFile> files = productDTO.getFiles();
-        List<String> uploadFileNames = FileUtil.saveFiles(files);
-
-        productDTO.setUploadFileNames(uploadFileNames);
-
-        log.info("[MY LOG] : loadFileNames: "+ productDTO.getUploadFileNames());
-
-        productService.register(productDTO);
-        return Map.of("Result", "SUCCESS");
-
-    }
 
     @GetMapping("/view/{filename}")
     public ResponseEntity<Resource> viewFileGet(@PathVariable String filename) {
@@ -54,10 +38,71 @@ public class ProductController {
         return Map.of("Result", "DELETE" + filename + "Success");
     }
 
-
     @GetMapping("/list")
     public PageResponseDTO<ProductDTO> getList(PageRequestDTO pageRequestDTO) {
         return productService.getList(pageRequestDTO);
     }
 
+    @PostMapping
+    public Map<String, Long> register(ProductDTO productDTO) {
+
+        log.info("[My LOG] : register " + productDTO);
+
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> uploadFileNames = FileUtil.saveFiles(files);
+
+        productDTO.setUploadFileNames(uploadFileNames);
+
+        log.info("[MY LOG] : loadFileNames: " + productDTO.getUploadFileNames());
+
+        Long register = productService.register(productDTO);
+        return Map.of("Result", register);
+
+    }
+
+    //조회
+    @GetMapping("/{pno}")
+    public ProductDTO getOne(@PathVariable Long pno) {
+
+        ProductDTO dto = productService.get(pno);
+
+        return dto;
+    }
+
+    //수정 및 삭제
+    @PutMapping("/{pno}")
+    public Map<String, String> PutModify(@PathVariable Long pno, ProductDTO productDTO) {
+
+        ProductDTO oldProductDTO = productService.get(pno);
+        log.info("[old]" + oldProductDTO);
+
+        productDTO.setPno(pno);
+
+        //파일 저장
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> currentFileUploadFileNames = FileUtil.saveFiles(files);
+
+        //기존 저장할 파일 이름
+        List<String> uploadFileNames = productDTO.getUploadFileNames();
+        if (currentFileUploadFileNames != null) {
+            uploadFileNames.addAll(currentFileUploadFileNames);
+        }
+
+        // 위 내용 까지는 imageList에 사진이 저장되어있다.
+        List<String> oldUploadFileNames = oldProductDTO.getUploadFileNames();
+
+        log.info("[old]:" + oldUploadFileNames);
+
+        if (oldUploadFileNames != null) {
+            List<String> removeFiles =  oldUploadFileNames
+                    .stream()
+                    .filter(fileName -> uploadFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
+
+            FileUtil.delelteFiles(removeFiles);
+        }
+
+        productService.modify(productDTO);
+
+        return Map.of("Result", "Success");
+    }
 }
--- mall/src/main/java/com/eedo/mall/domain/service/ProductService.java
+++ mall/src/main/java/com/eedo/mall/domain/service/ProductService.java
@@ -4,6 +4,9 @@ import com.eedo.mall.domain.dto.PageRequestDTO;
 import com.eedo.mall.domain.dto.PageResponseDTO;
 import com.eedo.mall.domain.dto.ProductDTO;
 import com.eedo.mall.domain.entity.Product;
+import com.eedo.mall.domain.entity.ProductImage;
+
+import java.util.List;
 
 public interface ProductService {
 
@@ -22,13 +25,26 @@ public interface ProductService {
 
     default ProductDTO entityToDTO(Product product) {
 
-        return ProductDTO.builder()
+        ProductDTO productDTO = ProductDTO.builder()
                 .pno(product.getPno())
                 .pname(product.getPname())
                 .pdesc(product.getPdesc())
                 .delFlag(product.isDelFlag())
                 .build();
+
+        List<ProductImage> imageList = product.getImageList();
+        if (imageList.isEmpty()) {
+            return productDTO;
+        }
+
+        List<String> fileName = imageList.stream().map(
+                productImage -> productImage.getFileName()).toList();
+
+        productDTO.setUploadFileNames(fileName);
+
+        return productDTO;
     }
+
 
     default Product dtoToEntity(ProductDTO productDTO) {
 
