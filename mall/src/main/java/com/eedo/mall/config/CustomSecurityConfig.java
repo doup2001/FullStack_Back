45a5c0e9681d9acd50fdddf538fa8b263a928554
eedo_y <doup2001@naver.com>
2024-09-17 12:02:25 +0900

    fix : 스프링시큐리티 config파일 비밀번호 설정

--- mall/src/main/java/com/eedo/mall/config/CustomSecurityConfig.java
+++ mall/src/main/java/com/eedo/mall/config/CustomSecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -41,4 +43,10 @@ public class CustomSecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    };
+
 }
